1e25e79ba34778162b690e3a586a7073a54a2f62
alisonfrd <alisonfrd@outlook.com>
2022-09-03 12:12:36 -0300

    aula 2 concluida

---
+++ Declarando-variaveis/truthy_falsy.js
@@ -0,0 +1,29 @@
+//boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+//0 => false
+//1 => true 
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+//null
+// undefined
+
+let valorNull = null;
+let valorUndefined;
+
+console.log(valorNull);
+console.log(valorUndefined);
+
+console.log(typeof valorNull);
+console.log(typeof valorUndefined);
+
+let numero = 3;
+let texto = 'Backend';
+
+console.log(typeof numero);
+console.log(typeof texto);

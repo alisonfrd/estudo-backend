64636ddd6c3810714d17de28c577a42a07f33c6b
alisonfrd <alisonfrd@outlook.com>
2022-09-03 11:57:59 -0300

    aula concluida

---
+++ Declarando-variaveis/var_let_const.js
@@ -0,0 +1,33 @@
+//var == primeira, funciona em qualquer parte do código
+//let == sugiram a partir da versao 2015, 
+//const == constate ,inalteravel
+
+// var altura = 5;
+// var comprimento = 10;
+
+// area = altura * comprimento;
+// console.log(area);
+// var area;
+
+// let forma = 'ret';
+// let altura = 5
+// let comprimento = 10;
+// let area;
+// if (forma === 'ret') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+// console.log(area);
+
+const forma = 'qua';
+const altura = 5;
+const largura = 10;
+let total;
+
+if (forma === 'qua') {
+    total = altura * largura;
+} else {
+    total = (altura * largura) / 2;
+}
+console.log(total);
